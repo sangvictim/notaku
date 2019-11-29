deb7b5eca56343a69d9b8906893915f5cd04d918
sangvictim <sangvictim@gmail.com>
2019-11-29 15:50:59 +0700

    add template

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,35 @@
 require('./bootstrap');
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+import App from './views/App'
+import Welcome from './views/Welcome'
+
+const router = new VueRouter({
+    // mode: 'history',
+    routes: [
+        {
+            path: '/',
+            // name: 'home',
+            redirect: '/welcome'
+        },
+        {
+            path: '/dashboard',
+            // name: 'home',
+            component: Welcome
+        },
+        {
+            path: '/tes',
+            // name: 'home',
+            component: Welcome
+        },
+    ],
+});
+
+const app = new Vue({
+    el: '#app',
+    components: { App },
+    router,
+});
