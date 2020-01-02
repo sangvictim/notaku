bc3b6ee81e189d3ed91e4021ba4478b5dcc2e9fa
sangvictim <sangvictim@gmail.com>
2020-01-02 10:49:09 +0700

    revisi route web

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,8 @@
 |
 */
 
-// Route::get('/', function () {
-//     return view('login');
-// });
+Route::get('/', function () {
+    return view('login');
+});
 
 Route::get('/page', 'SinglePageController@index');
