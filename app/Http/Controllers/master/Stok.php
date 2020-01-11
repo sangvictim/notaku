b63918ae03f21607636ddb1308aaa9ba58122a18
sangvictim <sangvictim@gmail.com>
2020-01-11 20:53:58 +0700

    revisi stok

--- app/Http/Controllers/master/Stok.php
+++ app/Http/Controllers/master/Stok.php
@@ -11,14 +11,9 @@ class Stok extends Controller
 {
     public function index()
     {
-        $data = [];
         $data = M_stok::select('master_stok.*', 'master_barang.name')
                         ->join('master_barang', 'master_stok.kode', 'master_barang.kode')
                         ->get();
-        foreach ($data as $key => $value) {
-            $konversi = M_konversi::where('kode', $value->kode)->get();
-            $data['satuan'] = $konversi; 
-        }
         return response()->json([
             'status' => '200 OK',
             'result' => $data,
